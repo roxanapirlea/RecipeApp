2957c1259e2706527f7deaf1efec887bfd22d302
roxanapirlea <roxanap.dev@gmail.com>
2021-12-19 16:56:09 +0100

    Update home screen

--- app/src/main/java/com/roxana/recipeapp/home/HomeScreen.kt
+++ app/src/main/java/com/roxana/recipeapp/home/HomeScreen.kt
@@ -3,15 +3,19 @@ package com.roxana.recipeapp.home
 import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.Icon
 import androidx.compose.material.Scaffold
+import androidx.compose.material.SnackbarDuration
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Add
+import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.roxana.recipeapp.R
+import com.roxana.recipeapp.home.ui.ContentView
 import com.roxana.recipeapp.home.ui.EmptyView
 import com.roxana.recipeapp.misc.rememberFlowWithLifecycle
 import com.roxana.recipeapp.ui.AppBar
@@ -31,6 +35,9 @@ fun HomeScreen(
     HomeView(state, homeViewModel.sideEffectFlow) { action ->
         when (action) {
             AddRecipe -> onNavAddRecipe()
+            is RecipeDetail -> {
+                // no-op
+            }
         }
     }
 }
@@ -41,13 +48,22 @@ fun HomeView(
     sideEffectsFlow: Flow<HomeSideEffect> = flow { },
     onAction: (HomeViewAction) -> Unit = {}
 ) {
+    val scaffoldState = rememberScaffoldState()
+    val localContext = LocalContext.current.applicationContext
+
     LaunchedEffect(sideEffectsFlow) {
-        sideEffectsFlow.collect {
-            // no-op
+        sideEffectsFlow.collect { sideEffect ->
+            when (sideEffect) {
+                ItemsFetchingError -> scaffoldState.snackbarHostState.showSnackbar(
+                    message = localContext.getString(R.string.home_recipe_fetch_error),
+                    duration = SnackbarDuration.Short
+                )
+            }
         }
     }
 
     Scaffold(
+        scaffoldState = scaffoldState,
         topBar = {
             AppBar(title = stringResource(R.string.home_title))
         },
@@ -62,6 +78,7 @@ fun HomeView(
     ) {
         when (state) {
             HomeViewState.Empty -> EmptyView()
+            is HomeViewState.Content -> ContentView(state, onAction)
         }
     }
 }
