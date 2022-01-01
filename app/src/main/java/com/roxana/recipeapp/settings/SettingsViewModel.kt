369726b828e3bf0e219cc30f40c5ccf9942542e4
roxanapirlea <roxanap.dev@gmail.com>
2022-01-01 20:05:41 +0200

    Get settings from preferrences

--- app/src/main/java/com/roxana/recipeapp/settings/SettingsViewModel.kt
+++ app/src/main/java/com/roxana/recipeapp/settings/SettingsViewModel.kt
@@ -4,19 +4,30 @@ import androidx.annotation.VisibleForTesting
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.roxana.recipeapp.domain.quantities.GetAllQuantityTypesUseCase
+import com.roxana.recipeapp.domain.quantities.GetPreferredQuantitiesUseCase
+import com.roxana.recipeapp.domain.quantities.SavePreferredQuantitiesUseCase
+import com.roxana.recipeapp.domain.temperature.GetPreferredTemperatureUseCase
+import com.roxana.recipeapp.domain.temperature.SavePreferredTemperatureUseCase
 import com.roxana.recipeapp.uimodel.UiQuantityType
 import com.roxana.recipeapp.uimodel.UiTemperature
+import com.roxana.recipeapp.uimodel.toDomainModel
 import com.roxana.recipeapp.uimodel.toUiModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class SettingsViewModel @Inject constructor(
     private val quantityTypesUseCase: GetAllQuantityTypesUseCase,
+    private val preferredQuantitiesUseCase: GetPreferredQuantitiesUseCase,
+    private val preferredTemperatureUseCase: GetPreferredTemperatureUseCase,
+    private val savePreferredQuantitiesUseCase: SavePreferredQuantitiesUseCase,
+    private val savePreferredTemperatureUseCase: SavePreferredTemperatureUseCase
 ) : ViewModel() {
     @VisibleForTesting
     val _state = MutableStateFlow(SettingsViewState())
@@ -24,24 +35,40 @@ class SettingsViewModel @Inject constructor(
 
     init {
         viewModelScope.launch {
-            val measuringUnits = quantityTypesUseCase(null).getOrNull()?.let { quantityTypes ->
-                quantityTypes.map { MeasuringUnit(it.toUiModel()) }
-            } ?: emptyList()
-            _state.value = state.value.copy(
-                measuringUnits = measuringUnits
-            )
+            combine(
+                preferredTemperatureUseCase(null),
+                preferredQuantitiesUseCase(null)
+            ) { temperature, units ->
+                val preferredUnits = units.getOrNull() ?: emptyList()
+                val allUnits = quantityTypesUseCase(null).getOrNull()?.let { quantityTypes ->
+                    quantityTypes.map { MeasuringUnit(it.toUiModel(), preferredUnits.contains(it)) }
+                } ?: emptyList()
+                val preferredTemperature =
+                    temperature.getOrNull()?.toUiModel() ?: UiTemperature.Celsius
+                SettingsViewState(
+                    measuringUnits = allUnits,
+                    temperatures = listOf(UiTemperature.Celsius, UiTemperature.Fahrenheit),
+                    selectedTemperature = preferredTemperature
+                )
+            }.collect { state ->
+                _state.value = state
+            }
         }
     }
 
     fun onTemperatureSelected(type: UiTemperature) {
-        _state.value = state.value.copy(selectedTemperature = type)
+        viewModelScope.launch {
+            savePreferredTemperatureUseCase(type.toDomainModel())
+        }
     }
 
     fun onMeasuringUnitChanged(unit: UiQuantityType, isChecked: Boolean) {
-        _state.value = state.value.copy(
-            measuringUnits = state.value.measuringUnits.map {
-                if (it.unit == unit) it.copy(isChecked = isChecked) else it
-            }
-        )
+        val measuringUnits = state.value.measuringUnits
+            .map { if (it.unit == unit) it.copy(isChecked = isChecked) else it }
+            .filter { it.isChecked }
+            .mapNotNull { it.unit.toDomainModel() }
+        viewModelScope.launch {
+            savePreferredQuantitiesUseCase(measuringUnits)
+        }
     }
 }
