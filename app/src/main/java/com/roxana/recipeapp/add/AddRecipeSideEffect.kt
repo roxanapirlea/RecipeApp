573a7891b052953e9d10952a05eea5e8dd9fd39f
roxanapirlea <roxanap.dev@gmail.com>
2022-05-31 08:50:33 +0200

    Delete previous add recipe view

--- app/src/main/java/com/roxana/recipeapp/add/AddRecipeSideEffect.kt
+++
@@ -1,7 +0,0 @@
-package com.roxana.recipeapp.add
-
-sealed class AddRecipeSideEffect
-object ShowCategoryError : AddRecipeSideEffect()
-object ShowQuantityError : AddRecipeSideEffect()
-object SaveRecipeError : AddRecipeSideEffect()
-object SaveRecipeSuccess : AddRecipeSideEffect()
--- app/src/main/java/com/roxana/recipeapp/add/StateToRecipeMapper.kt
+++ app/src/main/java/com/roxana/recipeapp/add/StateToRecipeMapper.kt
@@ -1,6 +1,5 @@
 package com.roxana.recipeapp.add
 
-import androidx.annotation.VisibleForTesting
 import com.roxana.recipeapp.add.recap.RecapViewState
 import com.roxana.recipeapp.domain.model.CreationComment
 import com.roxana.recipeapp.domain.model.CreationIngredient
@@ -8,32 +7,6 @@ import com.roxana.recipeapp.domain.model.CreationInstruction
 import com.roxana.recipeapp.domain.model.CreationRecipe
 import com.roxana.recipeapp.uimodel.UiQuantityType
 import com.roxana.recipeapp.uimodel.toDomainModel
-
-fun AddRecipeViewState.toRecipe(): CreationRecipe {
-    return CreationRecipe(
-        name = title.text,
-        photoPath = null,
-        categories = categories.filter { it.isSelected }.map { it.type.toDomainModel() },
-        portions = portions.value,
-        instructions = instructions
-            .filter { it.fieldState.text.isNotEmpty() }
-            .mapIndexed { index, instr ->
-                CreationInstruction(instr.fieldState.text, index.toShort())
-            },
-        ingredients = ingredients
-            .filter { !it.isEmpty }
-            .map { ingredient -> ingredient.toDomainModel() },
-        timeTotal = time.total.value,
-        timePreparation = time.preparation.value,
-        timeCooking = time.cooking.value,
-        timeWaiting = time.waiting.value,
-        temperature = temperature.value,
-        temperatureUnit = temperature.value?.let { temperatureUnit.toDomainModel() },
-        comments = comments
-            .filter { it.fieldState.text.isNotEmpty() }
-            .mapIndexed { index, comm -> CreationComment(comm.fieldState.text, index.toShort()) }
-    )
-}
 
 fun RecapViewState.toRecipe(): CreationRecipe {
     return CreationRecipe(
@@ -59,10 +32,4 @@ fun RecapViewState.toRecipe(): CreationRecipe {
         comments = comments
             .mapIndexed { index, comm -> CreationComment(comm, index.toShort()) }
     )
-}
-
-@VisibleForTesting
-fun IngredientState.toDomainModel(): CreationIngredient {
-    val type = if (quantity.value == null) UiQuantityType.None else quantityType
-    return CreationIngredient(name.text, quantity.value, type.toDomainModel())
 }
