5e6ff9333da2972a108e32f76cffa939efcc39d1
roxanapirlea <roxanap.dev@gmail.com>
2022-06-14 08:47:22 +0200

    Delete no longer used view action

--- app/src/main/java/com/roxana/recipeapp/edit/recap/RecapViewAction.kt
+++
@@ -1,8 +0,0 @@
-package com.roxana.recipeapp.edit.recap
-
-sealed class RecapViewAction
-
-object Back : RecapViewAction()
-object Edit : RecapViewAction()
-object Save : RecapViewAction()
-object Finish : RecapViewAction()
