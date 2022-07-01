ffa1d52c5526060e9514cea3ce5014180c18dadc
roxanapirlea <roxanap.dev@gmail.com>
2022-07-01 22:47:57 +0300

    Update buttons layout

--- app/src/main/java/com/roxana/recipeapp/edit/SaveCreationDialog.kt
+++ app/src/main/java/com/roxana/recipeapp/edit/SaveCreationDialog.kt
@@ -1,7 +1,10 @@
 package com.roxana.recipeapp.edit
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.AlertDialog
 import androidx.compose.material.Text
@@ -10,6 +13,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.roxana.recipeapp.R
 import com.roxana.recipeapp.ui.FlatSecondaryButton
@@ -29,14 +33,22 @@ fun SaveCreationDialog(
                 onDismiss()
             },
             title = {
-                Text(stringResource(R.string.edit_recipe_dialog_title))
+                Text(
+                    stringResource(R.string.edit_recipe_dialog_title),
+                    textAlign = TextAlign.Center
+                )
             },
             text = {
-                Text(stringResource(R.string.edit_recipe_dialog_message))
+                Text(
+                    stringResource(R.string.edit_recipe_dialog_message),
+                    textAlign = TextAlign.Center
+                )
             },
             buttons = {
                 Row(
-                    modifier = Modifier.padding(8.dp),
+                    modifier = Modifier
+                        .height(IntrinsicSize.Min)
+                        .padding(8.dp),
                     horizontalArrangement = Arrangement.End
                 ) {
                     FlatSecondaryButton(
@@ -44,18 +56,30 @@ fun SaveCreationDialog(
                             openDialog.value = false
                             onDelete()
                         },
-                        modifier = Modifier.padding(horizontal = 8.dp)
+                        modifier = Modifier
+                            .padding(horizontal = 8.dp)
+                            .weight(1f)
+                            .fillMaxHeight()
                     ) {
-                        Text(stringResource(R.string.edit_recipe_dialog_reset_info))
+                        Text(
+                            stringResource(R.string.edit_recipe_dialog_reset_info),
+                            textAlign = TextAlign.Center
+                        )
                     }
                     FlatSecondaryButton(
                         onClick = {
                             openDialog.value = false
                             onSave()
                         },
-                        modifier = Modifier.padding(horizontal = 8.dp)
+                        modifier = Modifier
+                            .padding(horizontal = 8.dp)
+                            .weight(1f)
+                            .fillMaxHeight()
                     ) {
-                        Text(stringResource(R.string.edit_recipe_dialog_keep_recipe))
+                        Text(
+                            stringResource(R.string.edit_recipe_dialog_keep_recipe),
+                            textAlign = TextAlign.Center
+                        )
                     }
                 }
             }
