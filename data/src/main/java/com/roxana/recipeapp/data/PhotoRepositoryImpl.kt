0b1d8e1afe834e95c0850889753ed2f088543745
roxanapirlea <roxanap.dev@gmail.com>
2022-07-06 15:04:20 +0300

    Add photo path to the db

--- data/src/main/java/com/roxana/recipeapp/data/PhotoRepositoryImpl.kt
+++ data/src/main/java/com/roxana/recipeapp/data/PhotoRepositoryImpl.kt
@@ -6,9 +6,11 @@ import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import java.io.File
+import java.time.Instant
 import javax.inject.Inject
 
 private const val PHOTOS_PATH = "photo"
+private const val PHOTO_PREFIX = "photo_"
 
 class PhotoRepositoryImpl @Inject constructor(
     @ApplicationContext val context: Context
@@ -16,16 +18,18 @@ class PhotoRepositoryImpl @Inject constructor(
 
     private val absoluteDirPath = "${context.filesDir}/$PHOTOS_PATH"
 
-    override suspend fun copyTempFileToPermFile(tempPath: String, permFileName: String): String? =
+    override suspend fun copyTempFileToPermFile(tempPath: String): String? =
         withContext(Dispatchers.IO) {
             kotlin.runCatching {
                 val tempFile = File(tempPath)
-                val newFile = createFile(absoluteDirPath, permFileName) ?: return@runCatching null
+                val newFile = createFile(absoluteDirPath, getFileName()) ?: return@runCatching null
                 tempFile.copyTo(newFile, true)
                 tempFile.delete()
                 newFile.absolutePath
             }.getOrNull()
         }
+
+    private fun getFileName(): String = "$PHOTO_PREFIX${Instant.now().epochSecond}"
 
     private suspend fun createFile(path: String, name: String): File? =
         withContext(Dispatchers.IO) {
--- domain/src/main/java/com/roxana/recipeapp/domain/PhotoRepository.kt
+++ domain/src/main/java/com/roxana/recipeapp/domain/PhotoRepository.kt
@@ -1,6 +1,6 @@
 package com.roxana.recipeapp.domain
 
 interface PhotoRepository {
-    suspend fun copyTempFileToPermFile(tempPath: String, permFileName: String): String?
+    suspend fun copyTempFileToPermFile(tempPath: String): String?
     suspend fun deleteFile(path: String)
 }
